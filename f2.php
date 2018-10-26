6b31b590c132b9e924d5db50bafc4648dd410c9d
Tomáš Brada <tomasbrod@azet.sk>
2018-10-27 00:16:36 +0200

    Coinbase hashboinc dissection, superblock disection, message disection. Formatting.

--- f2.php
+++ f2.php
@@ -9,21 +9,125 @@ function fragTransactionDetail($blockver,$index,$tx)
 
 function toKiSize($val)
 {
-	return $val;
+	return number_format($val,0,'.',"'");
+	/* Note: it does not look good with the size prefixes */
+	$tab=Array(
+		1073741824=>'Gi',
+		1048576=>'Mi',
+		1024=>'Ki',
+	);
+	foreach($tab as $base=>$prefix)
+	{
+		if($val>($base*8))
+		{
+			$a = floor($val / $base);
+			return $a.$prefix.'B';
+		}
+	}
+	return $val.'B';
+}
+
+function GRCExtractXML($data,$key)
+{
+	$matches=Array();
+	if(!preg_match("/\\<$key\\>(.*)\\<\\/$key\\>/",$data,$matches,PREG_UNMATCHED_AS_NULL))
+		return FALSE;
+	if($matches[1]===NULL)
+		return FALSE;
+	/*if($matches[1]==='')
+		return FALSE;*/
+	return $matches[1];
 }
 
 function fragCoinbaseHashboinc($version,$hashboinc)
 {
+	$arr=explode('<|>',$hashboinc);
 	?>
-			<p>dissect hashboinc of coinbase ...
+		<div class='kvboxes'>
+			<div><p>CPID<p><?=$arr[0]?></div>
+			<div><p>Client Version<p><?=$arr[10]?></div>
+			<div><p>Org/Nick<p><?=htmlspecialchars($arr[19])?></div>
+			<div><p>Interest<p><?=$arr[18]?></div>
+			<div><p>GRCAddress<p><?=$arr[16]?></div>
+			<?php /*same as prevblockhash <div><p>Last Block<p><a href='<?=href('block/'.$arr[17])?>'><?=substr($arr[17],0,10)?></a></div>*/?>
+			<div><p>NeuralHash<p><?=$arr[21]?></div>
+			<div><p>MagUnit<p><?=$arr[25]?></div>
+			<?php if($arr[0]!=='' && $arr[0]!='INVESTOR'):?>
+				<div><p>ResRwrd<p><?=$arr[11]?></div>
+				<div><p>Magnitude<p><?=$arr[15]?></div>
+				<div><p>ResAge<p><?=$arr[24]?></div>
+				<div><p>AvgMag<p><?=$arr[26]?></div>
+				<div><p>LastPORBlk<p><a href='<?=href('block/'.$arr[27])?>'><?=substr($arr[27],0,10)?></a></div>
+				<div><p>Beacon Public Key<p><?=$arr[29]?></div>
+			<?php endif;?>
+		</div>
 	<?php
+	if(!empty($arr[22])):
+		$sb=$arr[22];
+		$avg=GRCExtractXML($sb,'AVERAGES');
+		$avg=explode(';',$avg);
+		$prjcount=count($avg)-1; /* 1 is special NeuralNetwork */
+		?>
+		<div class='titlekvboxes'>
+			<div class='title'>Superblock</div>
+			<div class='kvboxes'>
+				<div><p>Size<p><?=toKiSize(strlen($arr[22]))?></div>
+				<div><p>Projects<p><?=$prjcount?></div>
+				<?php foreach($avg as $prjavg):
+					if($prjavg==='') continue;
+					$prjavg=explode(',',$prjavg);
+				?>
+					<div><p><?=$prjavg[0]?><p><?=$prjavg[1]?><p><?=$prjavg[2]?></div>
+				<?php endforeach;?>
+			</div>
+		</div>
+	<?php endif;
+}
+
+function fragHashboincGeneric($version,$hb)
+{
+	$action=GRCExtractXML($hb,'MA');
+	if($action=='A')
+		$action2='Add Contract';
+	elseif($action=='D')
+		$action2='Delete Contract';
+	else
+		return;
+	?>
+	<div class='titlekvboxes'>
+		<div class='title'><?=$action2?></div>
+		<div class='kvboxes'>
+			<div><p>Type<p><?=htmlspecialchars(GRCExtractXML($hb,'MT'))?></div>
+			<div><p>Key<p><?=htmlspecialchars(GRCExtractXML($hb,'MK'))?></div>
+			<?php if($action=='A'):?>
+				<div><p>Value<p><?=htmlspecialchars(GRCExtractXML($hb,'MV'))?></div>
+			<?php endif;?>
+		</div>
+	</div>
+	<?php
+	/* todo: public key */
 }
 
 function fragTxHashBoinc($version,$hashboinc)
 {
-	?>
-			<p>dissect hashboinc of transactions ...
-	<?php
+	//echo "<s>$hashboinc</s>";
+	$comment= GRCExtractXML($hashboinc,'MESSAGE');
+	$msgtype= GRCExtractXML($hashboinc,'MT');
+	$rainmsg= GRCExtractXML($hashboinc,'NARR');
+	if($comment!==false):?>
+	<div class='kvbox'>
+		<p>Comment
+		<p><?=htmlspecialchars($comment)?>
+	</div><?php elseif($rainmsg!==false):?>
+	<div class='kvbox'>
+		<p>Rain
+		<p><?=htmlspecialchars($rainmsg)?>
+	</div><?php elseif($msgtype!==false):
+		if($msgtype==='')
+			{echo "<s>test</s>";}
+		else
+			fragHashboincGeneric($version,$hashboinc);
+	endif;
 }
 
 function fragInputsOutputs($inputs,$outputs)
@@ -39,26 +143,33 @@ function fragInputsOutputs($inputs,$outputs)
 				<?php if(isset($in['txid'])): ?>
 					<div><a href='<?=href("tx/{$in['txid']}?i={$in['vout']}")?>'><?=substr($in['txid'],0,6)?>-<?=$in['vout']?></a></div>
 				<?php elseif(isset($in['coinbase'])): ?>
-					<div>coinbase</div>
+					<div>coinbase <?=$in['coinbase']?></div>
 				<?php endif;?>
 			<?php endforeach;?>
 		</div>
-		<?php if(!empty($outputs)):?><div class='txouttitle'>Outputs</div><div class='txout'>
+		<?php if(!empty($outputs)):?>
+			<table class='txout'><thead>
+				<tr><th colspan=1>Value (GRC)<th>Output</tr>
+			<thead><tbody>
 			<?php foreach($outputs as $out): ?>
-				<div><span><?=number_format($out['value'],8,".","'")?></span> <span>GRC</span>
+				<tr><td><?=number_format($out['value'],8,".","'")?><td>
 				<?php if(isset($out['scriptPubKey']) && isset($out['scriptPubKey']['type'])):
 					if($out['scriptPubKey']['type']=="pubkey"):	?>
 						<span>PK</span>
 						<span><?=$out['scriptPubKey']['addresses'][0]?></span>
 					<?php elseif($out['scriptPubKey']['type']=="pubkeyhash"):	?>
-						<span>PKH</span>
+						<span>pkh</span>
 						<span><?=$out['scriptPubKey']['addresses'][0]?></span>
+					<?php elseif($out['scriptPubKey']['type']=="scripthash"):	?>
+						<span>SH</span>
+						<span><?=$out['scriptPubKey']['asm'][0]?></span>
 					<?php else: ?>
-						<span>??</span>
+						<span>other</span>
+						<span><?=$out['scriptPubKey']['asm'][0]?></span>
 				<?php endif; endif; ?>
-				</div>
+				</tr>
 			<?php endforeach; ?>
-		</div><?php endif; ?>
+		</tbody></table><?php endif; ?>
 	</div>
 	<?php
 }
@@ -77,9 +188,6 @@ function fragBlock($block)
 	?>
 	<main>
 		<h2>Block Details</h2>
-		
-
-		<h3>Block Information</h3>
 		<div class='blockinfoheader'>
 			<a href='<?=href('block/'.$block->previousblockhash)?>'><span class='arrowback'>&lt;=</span></a>
 			<span class='hash'><?=$block->hash?></span>
@@ -87,10 +195,10 @@ function fragBlock($block)
 				<a href='<?=href('block/'.$block->nextblockhash)?>'><span class='arrowfwrd'>=&gt;</span></a>
 			<?php endif;?>
 		</div>
-		<div class='blockinfo'>
-			<div><p>Height<p><?=$block->height?></div>
+		<div class='blockinfo kvboxes'>
+			<div><p>Height<p><?=number_format($block->height,0,".","'")?></div>
 			<div><p>Difficulty<p><?=number_format($block->difficulty,4,'.','')?></div>
-			<div><p>Depth<p>...</div>
+			<div><p>Depth<p><?=$block->confirmations?></div>
 			<div><p>Size<p><?=toKiSize($block->size)?></div>
 			<div><p>Time<p><?=$block->time?></div>
 			<div><p>Ver<p><?=$block->version?></div>
@@ -116,50 +224,21 @@ function fragBlock($block)
 						<div><span>Locked: </span><span><?=$tx->locktime?></span></div>
 					<?php endif; ?>
 				</div>
-				<?php if(!empty($tx->hashboinc)) { fragTxHashBoinc($block->version,$tx->hashboinc); } ?>
 				<?php fragInputsOutputs($tx->vin,$tx->vout); ?>
+				<?php if(!empty($tx->hashboinc)) { fragTxHashBoinc($block->version,$tx->hashboinc); } ?>
 			</div><?php endfor;?>
 		<h3>Index extra</h3>
 		<a href="https://grctnexplorer.neuralminer.io/block/002d8055684709e730ac82e2457558f81c787a74d16f866dfc9df3d8421b9310">Inspiration</a>
-		<div class='blockinfox'>
+		<div class='blockinfox kvboxes'>
 			<div><p>Money Supply<p><?=$block->MoneySupply?></div>
 			<div><p>Mint<p><?=$block->mint?></div>
 			<div><p>Trust<p><?=$block->blocktrust?></div>
 			<?php if(isset($block->modifier)):?>
 				<div><p>Modifier<p><?=$block->modifier?></div>
 			<?php endif;?>
+			<div><p>Flags<p><?=$block->flags?></div>
+			<div><p>Proof Hash<p><?=$block->proofhash?></div>
 		</div>
-		<table><tbody>
-			<tr><th>Hash<td><?=$block->hash?>
-			<tr><th>Height<td><?=$block->height?>
-			<tr><th>Size<td><?=$block->size?>
-			<tr><th>Version<td><?=$block->version?>
-			<tr><th>Mint<td><?=$block->mint?>
-			<tr><th>MoneySupply<td><?=$block->MoneySupply?>
-			<tr><th>Time<td><?=$block->time?>
-			<tr><th>Difficulty<td><?=$block->difficulty?>
-			<tr><th>Block Trust<td><?=$block->blocktrust?>
-			<tr><th>Previous<td><a href='<?=href('block/'.$block->previousblockhash)?>'><?=$block->previousblockhash?></a>
-			<tr><th>Next<td><a href='<?=href('block/'.$block->nextblockhash)?>'><?=$block->nextblockhash?></a>
-			<tr><th>Confirmations<td><?=$block->confirmations?>
-			<tr><th>Flags<td><?=$block->flags?>
-			<tr><th>Proof Hash<td><?=$block->proofhash?>
-			<tr><th>Modifier<td><?=$block->modifier?>
-			<tr><th>CPID<td><?=$block->CPID?>
-			<tr><th>Magnitude<td><?=$block->Magnitude?>
-			<tr><th>Research<td><?=$block->ResearchSubsidy?>
-			<tr><th>LastPaymentTime<td><?=$block->LastPaymentTime?>
-			<tr><th>ResearchAge<td><?=$block->ResearchAge?>
-			<tr><th>ResearchMagnitudeUnit<td><?=$block->ResearchMagnitudeUnit?>
-			<tr><th>ResearchAverageMagnitude<td><?=$block->ResearchAverageMagnitude?>
-			<tr><th>LastPORBlockHash<td><?=$block->LastPORBlockHash?>
-			<tr><th>Interest<td><?=$block->Interest?>
-			<tr><th>Client Version<td><?=$block->ClientVersion?>
-			<tr><th>NeuralHash<td><?=$block->NeuralHash?>
-			<tr><th>IsSuperBlock<td><?=$block->IsSuperBlock?>
-			<tr><th>IsContract<td><?=$block->IsContract?>
-			</tbody>
-		</table>
 	</main>
 	<?php
 }
@@ -167,6 +246,31 @@ function fragBlock($block)
 function pageBlock($page,$rpc,$arg)
 {
 	$resp=$rpc->getblock($arg,true);
+	/*
+	switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+            echo ' - No errors';
+        break;
+        case JSON_ERROR_DEPTH:
+            echo ' - Maximum stack depth exceeded';
+        break;
+        case JSON_ERROR_STATE_MISMATCH:
+            echo ' - Underflow or the modes mismatch';
+        break;
+        case JSON_ERROR_CTRL_CHAR:
+            echo ' - Unexpected control character found';
+        break;
+        case JSON_ERROR_SYNTAX:
+            echo ' - Syntax error, malformed JSON';
+        break;
+        case JSON_ERROR_UTF8:
+            echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+        break;
+        default:
+            echo ' - Unknown error';
+        break;
+    }
+  */
 	fragBlock((object)$resp);
 	$page->durStatic();
 }
@@ -262,6 +366,7 @@ function pageRecent($page,$rpc,$arg)
 		<main>
 			<h2>Recent blocks</h2>
 			<p>Generated <?=fragRelaTime($data->reqtime);?>, last block <?=fragRelaTime($data->lastblock->time);?>.</p>
+			<p><a href=''>Refresh</a> (please do not use F5)</p>
 			<?php fragRecentBlocksTable($data,$moredetailed);?>
 		</main>
 	<?php
